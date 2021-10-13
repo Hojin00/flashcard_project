6d460500578885213ed3eb066b3548e1b759a5fe
João Brentano <joaobrentano.br@gmail.com>
2021-10-13 16:21:12 -0300

    Created Xcode project

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by João Brentano on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
