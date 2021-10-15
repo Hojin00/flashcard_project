4387a2f1e8676ae14e03fd768686c67e67bc4617
Hojin <khojin.ryu01@gmail.com>
2021-10-15 14:37:55 -0300

    share view created

--- FlashCards/ContentView.swift
+++ FlashCards/ContentView.swift
@@ -6,37 +6,19 @@
 //
 
 import SwiftUI
-import UIKit
+
 
 struct ContentView: View {
-    @State private var showDetails = false
+    
     
     var body: some View {
-        Button("Share app") {
-            self.showDetails = true
-            
+        VStack {
+            ShareView()
         }
-        .sheet(isPresented: $showDetails, onDismiss: {
-            print("Dismiss")
-        }, content: {
-            ActivityViewController(activityItems: [URL(string: "com.FlashCard://Deck?id=Ab3200b3")!], applicationActivities: nil)
-        })
     }
 }
 
-struct ActivityViewController: UIViewControllerRepresentable {
-    
-    var activityItems: [Any]
-    var applicationActivities: [UIActivity]? = nil
-    
-    func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityViewController>) -> UIActivityViewController {
-        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
-        return controller
-    }
-    
-    func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ActivityViewController>) {}
-    
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
