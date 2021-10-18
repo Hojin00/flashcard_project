6bee353d368b262634b0acbdd68d1c2d44212a16
lccxm <>
2021-10-18 15:20:54 -0300

    added audioManagers

---
+++ FlashCards/Model/RecordingModel.swift
@@ -0,0 +1,13 @@
+//
+//  RecordingModel.swift
+//  FlashCards
+//
+//  Created by Lucca Molon on 18/10/21.
+//
+
+import Foundation
+
+struct Recording {
+    let fileURL: URL
+    let createdAt: Date
+}
