d725029fa0054eabbebcbdba02f0f6ada378115e
Hojin <khojin.ryu01@gmail.com>
2021-10-21 14:08:50 -0300

    NotificationView & Cloudkit with Audio & Solve merge bugs

--- FlashCards/ContentView.swift
+++ FlashCards/ContentView.swift
@@ -13,78 +13,9 @@ import UIKit
 
 struct ContentView: View {
     
-    var notificationManager = NotificationManager()
-    let auxRecord = CKRecord.init(recordType: "FlashCard")
-    let auxRecord3 = CKRecord.init(recordType: "FlashCard")
-    let auxRecord2 = CKRecord.init(recordType: "Deck")
-    
-    @State var FlashCardReferenceList = [CKRecord.Reference]()
-    @State var flashCardList = [FlashCard]()
     
     var body: some View {
-        
-        VStack {
-            //            ShareView()
-            //
-            //            Button("auth") {
-            //                notificationManager.authorization()
-            //            }
-            //            Divider()
-            //            Button("aa") {
-            //                notificationManager.send(identifier: UUID().uuidString, title: "hello friend", body: "how r u? XD", hour: 16, minute: 45)
-            //                print("teste")
-            //                notificationManager.notificationCenter.getPendingNotificationRequests(completionHandler: { requests in
-            //                    for request in requests {
-            //                        print(request)
-            //                    }
-            //                }
-            //                )
-            //            }
-            //            Divider()
-            //            Button("delete all notifications") {
-            //                notificationManager.stopSending()
-            //            }
-            //
-            
-            
-            Button("create flashcard") {
-                
-                let auxFlashCard = FlashCard.init(myrecord: auxRecord, frontSideText: "frontText", frontSideImage: nil, backSideText: "backText", backSideImage: nil, category: "category", frontSideAudio: nil, backSideAudio: nil)
-                let auxFlashCard2 = FlashCard.init(myrecord: auxRecord3, frontSideText: "frontText", frontSideImage: nil, backSideText: "backText", backSideImage: nil, category: "category", frontSideAudio: nil, backSideAudio: nil)
-                CloudKitManager.shared.createFlashCard(flashCard: auxFlashCard.myrecord)
-                CloudKitManager.shared.createFlashCard(flashCard: auxFlashCard2.myrecord)
-                
-                flashCardList.append(auxFlashCard)
-                flashCardList.append(auxFlashCard2)
-                
-                let auxReference = CKRecord.Reference.init(recordID: auxFlashCard.myrecord.recordID, action: .none)
-                FlashCardReferenceList.append(auxReference)
-                
-                let auxReference2 = CKRecord.Reference.init(recordID: auxFlashCard2.myrecord.recordID, action: .none)
-                FlashCardReferenceList.append(auxReference2)
-                
-            }
-            
-            Button("create deck") {
-                
-                let auxDeck = Deck.init(myrecord: auxRecord2, flashcards: FlashCardReferenceList, title: "deck title", category: "deck category", reminderDate: Date())
-                
-                CloudKitManager.shared.createDeck(deck: auxDeck.myrecord)
-                
-            }
-            Button("fetch deck flashcards") {
-                
-                
-                CloudKitManager.shared.fetchDeck(deckID: CKRecord.ID.init(recordName: "DBEF706B-8DFC-473C-971B-E49BE3BEB807")) { Result in
-                    switch Result {
-                    case .success(let flashcard):
-                        print(flashcard)
-                    default:
-                        print("no decks")
-                    }
-                }
-            }
-        }
+        Text("Hello World")
     }
 }
 
@@ -95,3 +26,8 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
---
+++ FlashCards/FlashCard/Audio/AudioButtonView.swift
@@ -0,0 +1,96 @@
+//
+//  AudioButtonView.swift
+//  FlashCards
+//
+//  Created by Hojin Ryu on 20/10/21.
+//
+
+import Foundation
+import SwiftUI
+import CloudKit
+
+struct AudioButtonView: View {
+    
+    @ObservedObject var audioRecorder: AudioRecorder = AudioRecorder()
+    let auxRecord = CKRecord.init(recordType: "FlashCard")
+    let auxRecord3 = CKRecord.init(recordType: "FlashCard")
+    let auxRecord2 = CKRecord.init(recordType: "Deck")
+    
+    @State var FlashCardReferenceList = [CKRecord.Reference]()
+    @State var flashCardList = [FlashCard]()
+    
+    @State var recordAudio: CKAsset?
+    
+    var body: some View {
+        NavigationView{
+            VStack {
+                RecordingsListView(audioRecorder: audioRecorder)
+                
+                if audioRecorder.recording == false {
+                    Button(action: { self.audioRecorder.startRecording() }) {
+                        Image(systemName: "circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 100)
+                            .clipped()
+                            .foregroundColor(.red)
+                            .padding(.bottom, 40)
+                    }
+                } else {
+                    Button(action: {
+                        self.audioRecorder.stopRecording()
+                        
+                        guard let record = self.audioRecorder.auxRecordings?.fileURL else { return }
+                        recordAudio = CKAsset.init(fileURL: record)
+                    }) {
+                        Image(systemName: "stop.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 100)
+                            .clipped()
+                            .foregroundColor(.red)
+                            .padding(.bottom, 40)
+                    }
+                }
+                
+                Button("create flashcard") {
+                    
+                    let auxFlashCard = FlashCard.init(myrecord: auxRecord, frontSideText: "frontText", frontSideImage: nil, backSideText: "backText", backSideImage: nil, category: "category", frontSideAudio: recordAudio, backSideAudio: nil)
+                    
+                    CloudKitManager.shared.createFlashCard(flashCard: auxFlashCard.myrecord)
+                    
+                }
+                Button("create deck") {
+                    
+                    let auxDeck = Deck.init(myrecord: auxRecord2, flashcards: FlashCardReferenceList, title: "deck title", category: "deck category", reminderDate: Date())
+                    
+                    CloudKitManager.shared.createDeck(deck: auxDeck.myrecord)
+                    
+                }
+                Button("fetch deck flashcards") {
+                    
+                    
+                    CloudKitManager.shared.fetchDeck(deckID: CKRecord.ID.init(recordName: "DBEF706B-8DFC-473C-971B-E49BE3BEB807")) { Result in
+                        switch Result {
+                        case .success(let flashcard):
+                            print(flashcard)
+                        default:
+                            print("no decks")
+                        }
+                    }
+                }
+            }
+        }
+        .navigationBarTitle("Voice recorder")
+        .navigationBarItems(trailing: EditButton())
+    }
+}
+
+struct AudioButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        AudioButtonView()
+    }
+}
+
+
+
