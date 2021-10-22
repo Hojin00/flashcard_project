2b43b68944c0e1f748b293734d9ec1257eed753d
Hojin <khojin.ryu01@gmail.com>
2021-10-22 14:40:38 -0300

    test

---
+++ FlashCards/FlashCard/Card/teste.swift
@@ -0,0 +1,20 @@
+//
+//  teste.swift
+//  FlashCards
+//
+//  Created by Hojin Ryu on 22/10/21.
+//
+
+import SwiftUI
+
+struct teste: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct teste_Previews: PreviewProvider {
+    static var previews: some View {
+        teste()
+    }
+}
