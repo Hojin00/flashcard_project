6baf66f125b320024ba6a090aeb81ba79938fd78
WillianAlbeche <willianalbeche@gmail.com>
2021-10-26 20:15:58 -0300

    divider

--- FlashCards/FlashCard/Home/HomeView.swift
+++ FlashCards/FlashCard/Home/HomeView.swift
@@ -9,109 +9,105 @@ import SwiftUI
 
 struct HomeView: View {
     
-    
-    
     @State var sortKey = 0
     
     var options = ["Last Seen", "Hardest","Alphabet","Biggest"]
     var body: some View {
         
-        VStack{
+        VStack {
             VStack{
-                HStack{
-                    Text("Your FlashCards Decks")
-                        .font(.title)
-                        .bold()
-                        .padding(.leading)
-                    Spacer()
-                }
-                
-                HStack {
-                    Text("Sort by: ")
-                    Picker("Sort by", selection: $sortKey) {
-                        ForEach(0..<options.count) {
-                            Text(self.options[$0])
-                                .foregroundColor(.black)
-                        }
-                        
+                    HStack{
+                        Text("Your FlashCards Decks")
+                            .font(.title)
+                            .bold()
+                            .padding(.leading)
+                        Spacer()
                     }
-                    .padding(.horizontal, 3)
-                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 1.0, saturation: 0.012, brightness: 0.869)/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    .font(.headline)
-                    .cornerRadius(5)
-                    .pickerStyle(MenuPickerStyle())
                     
-                    Spacer()
-                    Button("See all"){
-                        print("Print tudo")
-                    }
-                    .padding(.trailing)
-                }
-                .padding(.leading)
-                
-                HStack(){
-                    VStack(){
-                        
-                        Button {
-                            print("Image tapped!")
-                        } label: {
-                            ZStack {
-                                Image("buttonNewCard")
-                                HStack {
-                                    Image(systemName:"plus")
-                                        .resizable()
-                                        .foregroundColor(Color.black)
-                                        .scaledToFit()
-                                        .frame(width: 25, height: 25)
-                                    
-                                    Text("New \n Card")
-                                        .font(.system(size: 16.0))
-                                        .fontWeight(.bold)
-                                        .lineLimit(2)
-                                        .foregroundColor(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                                }
+                    HStack {
+                        Text("Sort by: ")
+                        Picker("Sort by", selection: $sortKey) {
+                            ForEach(0..<options.count) {
+                                Text(self.options[$0])
+                                    .foregroundColor(.black)
                             }
                             
                         }
+                        .padding(.horizontal, 3)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 1.0, saturation: 0.012, brightness: 0.869)/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                        .font(.headline)
+                        .cornerRadius(5)
+                        .pickerStyle(MenuPickerStyle())
                         
-                        Button {
-                            print("new deck")
-                        } label : {
-                            ZStack {
-                                Image("buttonNewDeck")
-                                HStack(alignment: .center, spacing: 0.0) {
-                                    Text("New \n Deck")
-                                        .font(.system(size: 16.0))
-                                        .fontWeight(.bold)
-                                        .lineLimit(2)
-                                        .foregroundColor(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                                    
-                                    Image(systemName:"plus")
-                                        .resizable()
-                                        .foregroundColor(Color.black)
-                                        .scaledToFit()
-                                        .frame(width: 25, height: 25)
-                                    
+                        Spacer()
+                        Button("See all"){
+                            print("Print tudo")
+                        }
+                        .padding(.trailing)
+                    }
+                    .padding(.leading)
+                    
+                    HStack(){
+                        VStack(){
+                            
+                            Button {
+                                print("Image tapped!")
+                            } label: {
+                                ZStack {
+                                    Image("buttonNewCard")
+                                    HStack {
+                                        Image(systemName:"plus")
+                                            .resizable()
+                                            .foregroundColor(Color.black)
+                                            .scaledToFit()
+                                            .frame(width: 25, height: 25)
+                                        
+                                        Text("New \n Card")
+                                            .font(.system(size: 16.0))
+                                            .fontWeight(.bold)
+                                            .lineLimit(2)
+                                            .foregroundColor(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                                    }
                                 }
-                                .offset(x: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/, y:  -10)
+                                
+                            }
+                            
+                            Button {
+                                print("new deck")
+                            } label : {
+                                ZStack {
+                                    Image("buttonNewDeck")
+                                    HStack(alignment: .center, spacing: 0.0) {
+                                        Text("New \n Deck")
+                                            .font(.system(size: 16.0))
+                                            .fontWeight(.bold)
+                                            .lineLimit(2)
+                                            .foregroundColor(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                                        
+                                        Image(systemName:"plus")
+                                            .resizable()
+                                            .foregroundColor(Color.black)
+                                            .scaledToFit()
+                                            .frame(width: 25, height: 25)
+                                        
+                                    }
+                                    .offset(x: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/, y:  -10)
+                                }
                             }
                         }
                     }
-                }
-                
-                Divider()
-            }
-            
+                    
+                    
+            }.background(Color(hue: 0.739, saturation: 0.422, brightness: 0.585))
+            Divider()
+                .frame(height: 10)
             VStack{
                 Text("Carrosel")
             }
         }
-        
-        
-        
-        
-        
+            
+  
     }
 }
 
