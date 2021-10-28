a8ffdf16422a83b2a50658c6259386ce41797b2c
lccxm <>
2021-10-28 15:50:24 -0300

    deck view v2

--- FlashCards/ContentView.swift
+++ FlashCards/ContentView.swift
@@ -14,10 +14,10 @@ import UIKit
 struct ContentView: View {
     @State static private var flashcardMock = FlashCard(myrecord: CKRecord.init(recordType: "FlashCard"), frontSideText: "front text", frontSideImage: nil, backSideText: "back text", backSideImage: nil, category: nil, frontSideAudio: nil, backSideAudio: nil)
     
-    @State static private var deckmock = Deck.init(myrecord: CKRecord.init(recordType: "Deck"), flashcards: [CKRecord.Reference.init(record: flashcardMock.myrecord, action: .none)], title: "deck title", category: "lk", reminderDate: Date())
+    static private var deckmock = Deck.init(myrecord: CKRecord.init(recordType: "Deck"), flashcards: [CKRecord.Reference.init(record: flashcardMock.myrecord, action: .none)], title: "deck title", category: "lk", reminderDate: Date())
     
     var body: some View {
-        DeckView(deck: ContentView.$deckmock)
+        DeckView(deck: ContentView.deckmock)
     }
 }
 
--- FlashCards/FlashCard/Deck/DeckView.swift
+++ FlashCards/FlashCard/Deck/DeckView.swift
@@ -10,37 +10,60 @@ import CloudKit
 
 struct DeckView: View {
     
-    @Binding var deck: Deck
+    var deck: Deck
+    let screenSize: CGSize = UIScreen.main.bounds.size
+    @State private var searchText: String = ""
+    @State var flashcards: [FlashCard] = []
     
+    init(deck: Deck) {
+        self.deck = deck
+        var auxFlashCard: [FlashCard] = []
+        CloudKitManager.shared.fetchDeck(deckID: deck.myrecord.recordID) { Result in
+            switch Result {
+            case .success(let flashcard):
+                auxFlashCard = flashcard
+                print(flashcard)
+            default:
+                print("error")
+            }
+        }
+        flashcards = auxFlashCard
+    }
+
     var body: some View {
         
-        var auxFlash: FlashCard? = nil
-        
-        
-        ScrollView {
-                VStack {
-                    
-                    Button("aaaaaaa") {
-    //                    print("aaaaaa")
-    //                    CloudKitManager.shared.fetchDeckFlashcards(flashcardID: (deck.flashcards?[0].recordID)!) { Result in
-    //                        switch Result {
-    //                        case .success(let flashcard):
-    //                            print(flashcard)
-    //                        default:
-    //                            print("no flashcards")
-    //                        }
-    //                    }
-                    }
+        //var auxFlash: FlashCard? = nil
+        ZStack {
+            Circle()
+                .ignoresSafeArea()
+                .frame(width: screenSize.width * 1.4, height: screenSize.width * 1.4)
+                .padding(.top, screenSize.width * -1.3)
+                .foregroundColor(.gray)
+                .opacity(0.5)
+            VStack() {
+                Text("Deck Cards")
+                    .font(.title).bold()
+                SearchBar(text: $searchText)
+                    .frame(width: screenSize.width * 0.95, height: screenSize.height * 0.05, alignment: .center)
+                ScrollView {
                     VStack {
-                        CardPreview(cardType: .newCard)
-                        CardPreview(cardType: .practiceCard)
-                        
-                        CardPreview(cardType: .informationCard)
-                        CardPreview(cardType: .normalCard)
-                    }
+                        HStack(alignment: .center) {
+                                VStack {
+                                    CardPreview(cardType: .practiceCard)
+                                    CardPreview(cardType: .newCard)
+                                }
+                            CardPreview(cardType: .informationCard, deck: deck)
+                            }
+                            ForEach(flashcards, id: \.self) { flashcard in
+                                CardPreview(cardType: .normalCard, flashcard: flashcard)
+                                }
+                        }
                 }
+            }
             
+            .padding()
         }
+        
     }
 }
 
@@ -48,75 +71,135 @@ struct CardPreview: View {
     let screenSize: CGSize = UIScreen.main.bounds.size
     var cardType: CardType
     var flashcard: FlashCard?
+    var deck: Deck?
     var body: some View {
-        
             ZStack {
-                
-                if cardType == .newCard {
-                    RoundedRectangle(cornerRadius: 20)
-                        .foregroundColor(.white)
-                        .shadow(radius: 5)
-                        .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.12)
-                        .padding(.all, 2)
-                    VStack {
-                        Image(systemName: "plus")
-                            .resizable()
-                            .frame(width: screenSize.width * 0.085, height: screenSize.height * 0.03, alignment: .center)
-                        Text("New Card")
-                            .padding(.top, screenSize.height * 0.01)
+               if cardType == .newCard {
+                    Button() {
+                        print("click")
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .foregroundColor(.white)
+                                .shadow(radius: 5)
+                                .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.12)
+                            .padding(.all, 2)
+                            VStack {
+                                Image(systemName: "plus")
+                                    .resizable()
+                                    .frame(width: screenSize.width * 0.06, height: screenSize.width * 0.05, alignment: .center)
+                                    .foregroundColor(.black)
+                                Text("New Card")
+                                    .padding(.top, screenSize.height * 0.01)
+                                    .foregroundColor(.black)
+                            }
+                            .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.12)
+                            .padding(.all, screenSize.width * 0.01)
+                        }
+                        
                     }
-                    .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.12)
                 } else if cardType == .practiceCard {
-                    RoundedRectangle(cornerRadius: 20)
-                        .foregroundColor(.white)
-                        .shadow(radius: 5)
-                        .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.12)
-                        .padding(.all, 2)
-                    VStack {
-                        Image(systemName: "square.stack")
-                            .resizable()
-                            .frame(width: screenSize.width * 0.055, height: screenSize.height * 0.032)
-                        Text("Practice Deck")
-                            .padding(.top, 6)
+                    Button() {
+                        print("clock")
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .foregroundColor(.white)
+                                .shadow(radius: 5)
+                                .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.12)
+                                .padding(.all, 2)
+                            VStack {
+                                Image(systemName: "square.stack")
+                                    .resizable()
+                                    .frame(width: screenSize.width * 0.055, height: screenSize.height * 0.032)
+                                    .foregroundColor(.black)
+                                Text("Practice Deck")
+                                    .padding(.top, 6)
+                                    .foregroundColor(.black)
+                            }
+                            .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.12)
+                            .padding(.all, screenSize.width * 0.02)
+                        }
                     }
-                    .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.12)
                 } else if cardType == .informationCard {
                     RoundedRectangle(cornerRadius: 20)
                         .foregroundColor(.white)
                         .shadow(radius: 5)
-                        .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.22)
+                        .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.26)
                         .padding(.all, 2)
                     VStack {
-                        Text("High Importance")
-                            .font(.custom("SFProDisplay-Light", size: 13))
-                        Text("Deck Title Here")
-                            .font(.custom("SFProDisplay-Bold", size: 13)).bold()
-                        Text("Remembered 14% easily")
-                            .font(.custom("SFProDisplay-Medium", size: 13))
-                        HStack {
-                            Button("aa") {
-                                print("aa")
+                        VStack {
+                            Spacer()
+                            Text("High importance")
+                                .font(.caption)
+                            Text("\(deck?.title ?? "Deck title here if to big ooooooooo")")
+                                .fixedSize(horizontal: false, vertical: true)
+                                .lineLimit(2)
+                                .padding(.vertical, screenSize.width * 0.01)
+                                .padding(.horizontal, screenSize.width * 0.015)
+                            Text("Remembered")
+                                .font(.caption)
+                            Text("77% easily")
+                                .font(.caption)
+                            Spacer()
+                                .onTapGesture {
+                                    print("hellou")
                             }
-                            Text("140 cards")
-                                .font(.custom("SFProDisplay-Light", size: 13))
                         }
-                            
+                        .onTapGesture {
+                            print("ha")
+                        }
+                        HStack {
+                            Button() {
+                                print("click")
+                            } label: {
+                                StandartButton()
+                            }
+                                .padding(.leading, screenSize.width * -0.06)
+                            Text("120 cards")
+                                .font(.caption)
+                        }
+                        .padding()
                     }
-                    .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.22)
+                    .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.26)
                 } else {
                     RoundedRectangle(cornerRadius: 20)
                         .foregroundColor(.white)
                         .shadow(radius: 5)
-                        .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.22)
-                        .padding(.all, 2)
+                        .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.26)
+                        .padding(.all, screenSize.width * 0.01)
                     VStack {
-                        
+                        VStack {
+                            Text("Card Title goes here if too big ohh")
+                                .fixedSize(horizontal: false, vertical: true)
+                                .lineLimit(2)
+                                .padding(.top, screenSize.width * 0.02)
+                                .padding(.bottom, screenSize.width * 0.04)
+                                .padding(.horizontal, screenSize.width * 0.015)
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: screenSize.width * 0.2, height: screenSize.width * 0.2)
+                                .foregroundColor(.gray)
+                                .opacity(0.5)
+                            
+                        }
+                        .onTapGesture {
+                            print("he")
+                        }
+                        HStack {
+                            Button() {
+                                print("hohoho")
+                            } label: {
+                                StandartButton()
+                                    .padding(.leading, screenSize.width * 0.03)
+                                    .padding(.bottom, screenSize.width * -0.02)
+                        }
+                            Spacer()
+                        }
                     }
+                    .frame(width: screenSize.width * 0.35, height: screenSize.height * 0.26)
+                    .padding(.all, screenSize.width * 0.01)
                 }
             }
-                
-         
-            
     }
 }
 
@@ -124,15 +207,30 @@ enum CardType {
     case newCard, practiceCard, normalCard, informationCard
 }
 
+struct StandartButton: View {
+    let screenSize: CGSize = UIScreen.main.bounds.size
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 5)
+                .foregroundColor(.gray)
+                .shadow(radius: 3)
+                .opacity(0.5)
+            Image(systemName: "ellipsis")
+                .foregroundColor(.black)
+        }
+        .frame(width: screenSize.width * 0.08, height: screenSize.width * 0.08)
+    }
+}
+
 
 struct DeckView_Previews: PreviewProvider {
     @State static private var flashcardMock = FlashCard.init(myrecord: CKRecord.init(recordType: "FlashCard"), frontSideText: "front text", frontSideImage: nil, backSideText: "back text", backSideImage: nil, category: nil, frontSideAudio: nil, backSideAudio: nil)
     
-    @State static private var deckmock = Deck.init(myrecord: CKRecord.init(recordType: "Deck"), flashcards: [CKRecord.Reference.init(record: flashcardMock.myrecord, action: .none)], title: "deck title", category: "lk", reminderDate: Date())
+    static private var deckmock = Deck.init(myrecord: CKRecord.init(recordType: "Deck"), flashcards: [CKRecord.Reference.init(record: flashcardMock.myrecord, action: .none)], title: "deck title hellou", category: "lk", reminderDate: Date())
 
     
         
     static var previews: some View {
-        DeckView(deck: $deckmock)
+        DeckView(deck: deckmock)
     }
 }
