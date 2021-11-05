4667804313010932866c26bc08020b3defe8b530
WillianAlbeche <willianalbeche@gmail.com>
2021-11-05 14:50:09 -0300

    CorrectBugs

---
+++ FlashCards/FlashCard/SlideFlashCard/CardViewSlide.swift
@@ -0,0 +1,185 @@
+//
+//  CardView.swift
+//  FlashCards
+//
+//  Created by Willian Magnum Albeche on 20/10/21.
+//
+
+import SwiftUI
+
+struct CardViewSlide: View {
+    @State private var translation: CGSize = .zero
+    @State private var swipeStatus: EasyHard = .none
+    
+    private var card: FlashCardTeste
+    private var onRemove: (_ user: FlashCardTeste) -> Void
+    
+    
+    
+    private var thresholdPercentage: CGFloat = 0.20 // when the user has draged 35% the width of the screen in either direction
+    
+    private enum EasyHard: Int {
+        case easy, hard, none
+    }
+    
+    init(card: FlashCardTeste, onRemove: @escaping (_ card: FlashCardTeste) -> Void) {
+        self.card = card
+        self.onRemove = onRemove
+        
+    }
+    
+    /// What percentage of our own width have we swipped
+    /// - Parameters:
+    ///   - geometry: The geometry
+    ///   - gesture: The current gesture translation value
+    private func getGesturePercentage(_ geometry: GeometryProxy, from gesture: DragGesture.Value) -> CGFloat {
+        gesture.translation.width / geometry.size.width
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            
+            
+            VStack {
+                ZStack(alignment: self.swipeStatus == .easy ? .topLeading : .topTrailing) {
+                    
+                    //                     Image(self.card.frontSideImage ?? "")
+                    //                        .resizable()
+                    //                        .aspectRatio(contentMode: .fill)
+                    //                        .frame(width: geometry.size.width, height: geometry.size.height * 0.75)
+                    //                        .clipped()
+                    VStack {
+                        HStack{
+                            Text("12/40")
+                                .font(.caption)
+                            
+                            Spacer()
+                            Text("High")
+                                .font(.caption)
+                            
+                        }
+                        .padding([.leading, .bottom, .trailing])
+                        VStack(alignment: .center) {
+                            Text("\(self.card.frontSideText ?? "") PUDIM")
+                                .font(.title)
+                                .bold()
+                                .foregroundColor(.gray)
+                        }
+                        VStack{
+                            Image("OPudim")
+                                .resizable()
+                                .scaledToFill()
+                                .cornerRadius(10)
+                                .frame(width: 200, height: 200)
+                        }
+                        
+                        HStack{
+                            
+                            Button(){
+                                
+                            }label: {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: 100, height: 40)
+                                    .foregroundColor(Color(hue: 0.293, saturation: 0.792, brightness: 0.891))
+                            }
+//                            Circle()
+//                                .foregroundColor(Color.red)
+//
+//                                .frame(width: 130, height: 130)
+//                                .background(Color.blue)
+//                                .clipShape(Circle())
+//                                .offset(x: -170, y: 60)
+//
+//                            Circle()
+//                                .foregroundColor(Color.green)
+//                                .frame(width: 130, height: 130)
+//                                .background(Color.blue)
+//                                .clipShape(Circle())
+//                                .offset(x: 170, y: 60)
+                        }
+//                        LinearGradient(gradient: Gradient(colors: [Color.init(#colorLiteral(red: 0.1764705926, green: 1, blue: 0.5607843399, alpha: 1)), Color.init(#colorLiteral(red: 0.9601020217, green: 0.9646142125, blue: 0.9777105451, alpha: 0))]), startPoint: .bottom, endPoint: .top)
+                    }
+                    if self.swipeStatus == .easy {
+                        Text("EASY")
+                            .font(.headline)
+                        
+                            .padding()
+                            .background(Color.green)
+                            .cornerRadius(10)
+                            .foregroundColor(Color.white)
+                        
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.green, lineWidth: 3.0)
+                            ).padding(24)
+                            .rotationEffect(Angle.degrees(-45))
+                    } else if self.swipeStatus == .hard {
+                        Text("HARD")
+                            .font(.headline)
+                        
+                            .padding()
+                            .background(Color.red)
+                            .cornerRadius(10)
+                            .foregroundColor(Color.white)
+                        
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.red, lineWidth: 3.0)
+                            ).padding(.top, 45)
+                            .rotationEffect(Angle.degrees(45))
+                    }
+                    
+                    
+                }
+                
+                
+                
+                
+                
+            }
+            .padding(.bottom)
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 5)
+            .animation(.interactiveSpring())
+            .offset(x: self.translation.width, y: 0)
+            .rotationEffect(.degrees(Double(self.translation.width / geometry.size.width) * 25), anchor: .bottom)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        self.translation = value.translation
+                        
+                        if (self.getGesturePercentage(geometry, from: value)) >= self.thresholdPercentage {
+                            self.swipeStatus = .easy
+                        } else if self.getGesturePercentage(geometry, from: value) <= -self.thresholdPercentage {
+                            self.swipeStatus = .hard
+                        } else {
+                            self.swipeStatus = .none
+                        }
+                        
+                    }.onEnded { value in
+                        // determine snap distance > 0.5 aka half the width of the screen
+                        if abs(self.getGesturePercentage(geometry, from: value)) > self.thresholdPercentage {
+                            self.onRemove(self.card)
+                        } else {
+                            self.translation = .zero
+                        }
+                    }
+            )
+            
+            
+        }
+    }
+}
+
+// 7
+struct CardViewSlide_Previews: PreviewProvider {
+    static var previews: some View {
+        CardViewSlide(card: FlashCardTeste(id: 01, frontSideText: "", frontSideImage: "", backSideText: "", backSideImage: "", category: "", frontSideAudio: "", backSideAudio: "", frontSideColor: "", backSideColor: ""),
+                      onRemove: { _ in
+            // do nothing
+        })
+            .frame(height: 400)
+            .padding()
+    }
+}
---
+++ FlashCards/FlashCard/SlideFlashCard/SlideView.swift
@@ -0,0 +1,132 @@
+//
+//  SlideView.swift
+//  FlashCards
+//
+//  Created by Willian Magnum Albeche on 20/10/21.
+//
+
+import SwiftUI
+
+struct FlashCardTeste: Hashable  {
+    //var description: String
+    
+    var id: Int
+    
+    var frontSideText: String?
+    var frontSideImage: String?
+    var backSideText: String?
+    var backSideImage: String?
+    var category: String?
+    var frontSideAudio: String?
+    var backSideAudio: String?
+    var frontSideColor: String?
+    var backSideColor: String?
+    
+}
+
+struct SlideView: View {
+    /// List of users EXEMPLO TROCAR DEPOIS POR FLASH CARD
+    @State var cards: [FlashCardTeste] = [
+        FlashCardTeste(id: 01, frontSideText: "", frontSideImage: "", backSideText: "", backSideImage: "", category: "", frontSideAudio: "", backSideAudio: "", frontSideColor: "", backSideColor: ""), FlashCardTeste(id: 02, frontSideText: "", frontSideImage: "", backSideText: "", backSideImage: "", category: "", frontSideAudio: "", backSideAudio: "", frontSideColor: "", backSideColor: ""), FlashCardTeste(id: 03, frontSideText: "", frontSideImage: "", backSideText: "", backSideImage: "", category: "", frontSideAudio: "", backSideAudio: "", frontSideColor: "", backSideColor: ""), FlashCardTeste(id: 04, frontSideText: "", frontSideImage: "", backSideText: "", backSideImage: "", category: "", frontSideAudio: "", backSideAudio: "", frontSideColor: "", backSideColor: ""),
+    
+    ]
+    
+    /// Return the CardViews width for the given offset in the array
+    /// - Parameters:
+    ///   - geometry: The geometry proxy of the parent
+    ///   - id: The ID of the current user
+    private func getCardWidth(_ geometry: GeometryProxy, id: Int) -> CGFloat {
+        let offset: CGFloat = CGFloat(cards.count - 1 - id)
+        return geometry.size.width - offset
+    }
+    private func getCardHeight(_ geometry: GeometryProxy, id: Int) -> CGFloat {
+        let offset: CGFloat = CGFloat(cards.count - 1 - id)
+        return geometry.size.height - offset
+    }
+    
+    /// Return the CardViews frame offset for the given offset in the array
+    /// - Parameters:
+    ///   - geometry: The geometry proxy of the parent
+    ///   - id: The ID of the current user
+    private func getCardOffset(_ geometry: GeometryProxy, id: Int) -> CGFloat {
+        return  CGFloat(cards.count - 5 - id) * -10
+    }
+    
+    private var maxID: Int {
+        return self.cards.map { $0.id }.max() ?? 0
+    }
+    
+    var body: some View {
+        VStack {
+            GeometryReader { geometry in
+                LinearGradient(gradient: Gradient(colors: [Color.init(#colorLiteral(red: 0.1764705926, green: 1, blue: 0.5607843399, alpha: 1)), Color.init(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]), startPoint: .bottom, endPoint: .top)
+                    .frame(width: geometry.size.width * 1.5, height: geometry.size.height)
+                    .background(Color.blue)
+                    .clipShape(Circle())
+                    .offset(x: -geometry.size.width / 4, y: -geometry.size.height / 2)
+                    
+                
+                VStack(alignment: .center, spacing: 0.0) {
+                    //DateView()
+                    ZStack {
+                        ForEach(self.cards, id: \.self) { card in
+                            Group {
+                                // Range Operator
+                                if (self.maxID - 3)...self.maxID ~= card.id {
+                                    CardViewSlide(card: card, onRemove: { removedUser in
+                                        // Remove that user from our array
+                                        self.cards.removeAll { $0.id == removedUser.id }
+                                    })
+                                        .animation(.spring())
+                                        .frame(width: self.getCardWidth(geometry, id: card.id), height: 500)
+                                        .offset(x: 0, y: self.getCardOffset(geometry, id: card.id))
+                                }
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+            }
+        }.padding()
+    }
+}
+
+//struct DateView: View {
+//    var body: some View {
+//        VStack {
+//            HStack {
+//                VStack(alignment: .leading) {
+//                    Text("Friday, 10th January")
+//                        .font(.title)
+//                        .bold()
+//                    Text("Today")
+//                        .font(.subheadline)
+//                        .foregroundColor(.gray)
+//                }
+//                Spacer()
+//            }.padding()
+//        }
+//        .background(Color.white)
+//        .cornerRadius(10)
+//        .shadow(radius: 5)
+//    }
+//}
+
+//struct Triangle: Shape {
+//    func path(in rect: CGRect) -> Path {
+//        var path = Path()
+//
+//        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+//        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+//
+//        return path
+//    }
+//}
+
+struct SlideView_Previews: PreviewProvider {
+    static var previews: some View {
+    SlideView()
+    }
+}
