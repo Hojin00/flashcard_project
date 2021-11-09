8375d3f1224b9fc5403e877a1261b9e82463a7c2
WillianAlbeche <willianalbeche@gmail.com>
2021-11-09 16:46:18 -0300

    home to home

--- FlashCards/ContentView.swift
+++ FlashCards/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
 
     var body: some View {
 //        SlideView(deck: Deck.init(record: CKRecord.init(recordType: "Deck")))
-        AudioButtonView()
+        HomeView()
     }
 }
 
