38cf618c0c3f0a89e76c355004b23c8262f88f84
João Brentano <joaobrentano.br@gmail.com>
2021-11-10 17:09:59 -0300

    Tentando consertar toggle dos botões

--- FlashCards/FlashCard/EditDeck/ThemePickerSingleThemeView.swift
+++ FlashCards/FlashCard/EditDeck/ThemePickerSingleThemeView.swift
@@ -11,28 +11,34 @@ struct ThemePickerSingleThemeView: View {
     
     @State var isSelected: Bool
     @Binding var selectedTheme: Int
-    let themeColor: Color = .green
+    @Binding var selectedThemes: [Bool]
+    let themeColor: Color
     let themeNum: Int
     
     var body: some View {
         Button {
-            if isSelected {
-                isSelected = false
-                selectedTheme = 0
-            } else {
-                isSelected = true
-                selectedTheme = themeNum
+            DispatchQueue.main.async {
+                if isSelected {
+                    selectedTheme = 0
+                    selectedThemes = [true, false, false, false, false, false, false]
+                    isSelected = false
+                } else {
+                    selectedTheme = themeNum
+                    selectedThemes = [false, false, false, false, false, false, false]
+                    selectedThemes[themeNum] = true
+                    isSelected = true
+                }
             }
         } label: {
             ZStack {
                 if isSelected {
                     Circle()
                         .foregroundColor(.black)
-                        .frame(width: UIScreen.main.bounds.width * 0.085, height: UIScreen.main.bounds.height * 0.085)
+                        .frame(width: UIScreen.main.bounds.width * 0.09, height: UIScreen.main.bounds.height * 0.09)
                 }
                 Circle()
                     .foregroundColor(.white)
-                    .frame(width: UIScreen.main.bounds.width * 0.07, height: UIScreen.main.bounds.height * 0.07)
+                    .frame(width: UIScreen.main.bounds.width * 0.08, height: UIScreen.main.bounds.height * 0.08)
                 Circle()
                     .foregroundColor(themeColor)
                     .frame(width: UIScreen.main.bounds.width * 0.06, height: UIScreen.main.bounds.height * 0.06)
@@ -46,8 +52,9 @@ struct ThemePickerSingleThemeView: View {
 struct ThemePickerSingleThemeView_Previews: PreviewProvider {
     
     @State static var selectedTheme: Int = 0
+    @State static var selectedThemes: [Bool] = [true, false, false, false, false, false, false]
     
     static var previews: some View {
-        ThemePickerSingleThemeView(isSelected: false, selectedTheme: $selectedTheme, themeNum: 1)
+        ThemePickerSingleThemeView(isSelected: false, selectedTheme: $selectedTheme, selectedThemes: $selectedThemes, themeColor: .green, themeNum: 1)
     }
 }
--- FlashCards/FlashCard/EditDeck/ThemePickerView.swift
+++ FlashCards/FlashCard/EditDeck/ThemePickerView.swift
@@ -10,20 +10,16 @@ import SwiftUI
 struct ThemePickerView: View {
     
     @State var selectedTheme: Int = 0
+    @State var selectedThemes: [Bool] = [true, false, false, false, false, false, false]
     
     var body: some View {
-//        HStack {
-//            Spacer()
-//            WeekdayPickerSingleDayView(isSelected: selectedDays[0], selectedDays: $selectedDays, title: "Mon", dayNum: 0)
-//            WeekdayPickerSingleDayView(isSelected: selectedDays[1], selectedDays: $selectedDays, title: "Tue", dayNum: 1)
-//            WeekdayPickerSingleDayView(isSelected: selectedDays[2], selectedDays: $selectedDays, title: "Wed", dayNum: 2)
-//            WeekdayPickerSingleDayView(isSelected: selectedDays[3], selectedDays: $selectedDays, title: "Thu", dayNum: 3)
-//            WeekdayPickerSingleDayView(isSelected: selectedDays[4], selectedDays: $selectedDays, title: "Fri", dayNum: 4)
-//            WeekdayPickerSingleDayView(isSelected: selectedDays[5], selectedDays: $selectedDays, title: "Sat", dayNum: 5)
-//            WeekdayPickerSingleDayView(isSelected: selectedDays[6], selectedDays: $selectedDays, title: "Sun", dayNum: 6)
-//            Spacer()
-//        }
-        Circle()
+        HStack {
+            Spacer()
+            ThemePickerSingleThemeView(isSelected: selectedThemes[1], selectedTheme: $selectedTheme, selectedThemes: $selectedThemes, themeColor: .red, themeNum: 1)
+            ThemePickerSingleThemeView(isSelected: selectedThemes[2], selectedTheme: $selectedTheme, selectedThemes: $selectedThemes, themeColor: .green, themeNum: 2)
+            ThemePickerSingleThemeView(isSelected: selectedThemes[3], selectedTheme: $selectedTheme, selectedThemes: $selectedThemes, themeColor: .blue, themeNum: 3)
+            Spacer()
+        }
     }
 }
 
